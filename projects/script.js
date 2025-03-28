dc91624da6e856d8947d3375d8243ba8d1444703
Mahad Kalam <mahadkalam1@proton.me>
2025-03-28 21:47:35 +0000

    Big bang

---
+++ projects/script.js
@@ -0,0 +1,27 @@
+const response = await fetch("/data/projects.json")
+const projects = await response.json()
+
+const projectsList = document.getElementById("projects")
+
+projects.forEach((project) => {
+    const projectCard = document.createElement("li")
+    projectCard.classList.add("project-card")
+
+    const projectTitle = document.createElement("h2")
+    projectTitle.textContent = project.title
+    projectCard.appendChild(projectTitle)
+
+    const projectDescription = document.createElement("p")
+    projectDescription.textContent = project.description
+    projectCard.appendChild(projectDescription)
+
+    if (project.link) {
+        const projectLink = document.createElement("a")
+        projectLink.href = project.link
+        projectLink.classList.add("underline")
+        projectLink.textContent = "↗ View Project"
+        projectCard.appendChild(projectLink)
+    }
+
+    projectsList.appendChild(projectCard)
+})
